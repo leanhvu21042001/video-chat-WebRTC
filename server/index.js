77c59ac1645d42386ded6c412b501d0b59944c21
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-09-27 08:54:47 +0700

    update cors origin link

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: ["https://web-rtc-video-chat-client.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
